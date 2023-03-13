8789d066cdd8cc904dce48772b7d969e6a712877
chukwuani ebube <chukwuanisteven@gmail.com>
2023-03-13 18:55:23 +0100

    Deploy

--- app/Components/Navbar.js
+++ app/Components/Navbar.js
@@ -212,7 +212,7 @@ export default function Navbar({ active, top, post, setter }) {
             </span>
 
             <span className="down-arrow">
-                <svg aria-label="Down chevron icon" className="arrow" color="#262626" fill="#262626" height="12" role="img" viewBox="0 0 24 24" width="12"><path d="M12 17.502a1 1 0 0 1-.707-.293l-9-9.004a1 1 0 0 1 1.414-1.414L12 15.087l8.293-8.296a1 1 0 0 1 1.414 1.414l-9 9.004a1 1 0 0 1-.707.293Z"></path></svg>
+                <svg aria-label="Down chevron icon" className="arrow" color="#262626" fill="#262626" height="16" role="img" viewBox="0 0 24 24" width="16"><path d="M12 17.502a1 1 0 0 1-.707-.293l-9-9.004a1 1 0 0 1 1.414-1.414L12 15.087l8.293-8.296a1 1 0 0 1 1.414 1.414l-9 9.004a1 1 0 0 1-.707.293Z"></path></svg>
             </span>
 
             <div className="logo-dropdown">
