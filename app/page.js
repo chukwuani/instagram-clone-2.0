fd235d2f79346378421f03b0ec126161b11d20a9
chukwuani ebube <chukwuanisteven@gmail.com>
2023-04-16 20:38:00 +0100

    Animation with framer motion added

--- app/page.js
+++ app/page.js
@@ -1,18 +1,14 @@
-"use client"
-import { useEffect, useState } from "react"
-import Homepage from "./Components/Homepage"
-import Loader from "./Components/Loader"
+"use client";
+import { useEffect, useState } from "react";
+import Homepage from "./Components/Homepage";
+import Loader from "./Components/Loader";
 
 export default function Home() {
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    setLoading(!loading)
-  }, [])
-  
-  return (
-    <main>
-      {loading ? <Loader /> : <Homepage />}
-    </main>
-  )
+    setLoading(!loading);
+  }, []);
+
+  return <main>{loading ? <Loader /> : <Homepage />}</main>;
 }
